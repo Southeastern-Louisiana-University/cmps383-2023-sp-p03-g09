8dc7c5bfa5fd420098ab40813937819563fd4255
Fastcat31 <jeremy.clay@selu.edu>
2023-03-21 16:50:20 -0500

    Fixed SQL Tables

---
+++ SP23.P03.Web/Migrations/20230317050707_SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SP23.P03.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class SalesRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "transactionName",
+                table: "SaleEvents",
+                newName: "TicketDestination");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TicketDestination",
+                table: "SaleEvents",
+                newName: "transactionName");
+        }
+    }
+}
