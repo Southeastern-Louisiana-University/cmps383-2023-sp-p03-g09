6c57e9829c2e1d76add421a7ca5d047164d8eece
Michaela Laitinen <kaylait2324@gmail.com>
2023-03-28 09:00:57 -0500

    PassConfig entity and Passenger entity completed

--- SP23.P03.Web/Features/Passengers/Passenger.cs
+++ SP23.P03.Web/Features/Passengers/Passenger.cs
@@ -1,6 +1,20 @@
-﻿namespace SP23.P03.Web.Features.Passengers
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using SP23.P03.Web.Features.Authorization;
+
+namespace SP23.P03.Web.Features.Passengers
 {
     public class Passenger
     {
+        //do we want to implement a max/min age for children and seniors for different ticket prices??
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public required virtual User Owner { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTimeOffset Birthday { get; set; }
+
+       
+        
     }
 }
