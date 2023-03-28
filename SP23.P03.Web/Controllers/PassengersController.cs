3d2111bd9c61c9614acf7cb5f71b93ea65071bfd
Michaela Laitinen <kaylait2324@gmail.com>
2023-03-28 09:24:15 -0500

    wip Passenger Controller

--- SP23.P03.Web/Controllers/PassengersController.cs
+++ SP23.P03.Web/Controllers/PassengersController.cs
@@ -1,6 +1,57 @@
-﻿namespace SP23.P03.Web.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Extensions;
+using SP23.P03.Web.Features.Authorization;
+using SP23.P03.Web.Features.Passengers;
+
+namespace SP23.P03.Web.Controllers
 {
-    public class PassengersController
+    [Route("api/passengers")]
+    [ApiController]
+    public class PassengersController : ControllerBase
     {
+        private readonly DbSet<Passenger> passengers;
+        private readonly DataContext dataContext;
+        private readonly UserManager<User> userManager;
+
+        public PassengersController(DataContext dataContext, UserManager<User> userManager)
+        {
+            passengers = dataContext.Set<Passenger>();
+            this.dataContext = dataContext;
+            this.userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+
+        public ActionResult<PassengerDto> GetPassengerById([FromRoute] int id)
+        {
+            var passenger = passengers.FirstOrDefault(x => x.Id == id);
+
+            if (passenger == null)
+            {
+                return NotFound();
+            }
+
+            if (!(User.IsInRole(RoleNames.Admin) || passenger.OwnerId == User.GetCurrentUserId()))
+            {
+                return Forbid();
+            }
+
+            var passengerDto = new PassengerDto
+            {
+                Id = passenger.Id,
+                OwnerId = passenger.OwnerId,
+                FirstName = passenger.FirstName,
+                LastName = passenger.LastName,
+                Birthday = passenger.Birthday,
+               //add age and age group??
+            };
+
+            return Ok(passengerDto);
+        }
     }
 }
