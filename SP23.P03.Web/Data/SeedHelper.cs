0a68e66e13b20d35eb35bd1a2fe82b8c07b3f1a7
Fastcat31 <jeremy.clay@selu.edu>
2023-04-20 00:05:04 -0500

    Seeded Data Train locations

--- SP23.P03.Web/Data/SeedHelper.cs
+++ SP23.P03.Web/Data/SeedHelper.cs
@@ -83,9 +83,252 @@ public static class SeedHelper
             dataContext.Set<TrainStation>()
                 .Add(new TrainStation
                 {
-                    Name = "Hammond",
-                    Address = "1234 Place st"
+                    Name = "EnTrack Hammond",
+                    Address = "Hammond LA"
                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+              {
+                  Name = "EnTrack New Orleans",
+                  Address = "New Orleans LA"
+              });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Slidell ",
+                   Address = "Slidell LA"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Picayune",
+                Address = "Picayune MS"
+            });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack Hattiesburg",
+                     Address = "Hattiesburg MS"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Laurel",
+                Address = "Laurel MS"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Biloxi",
+                   Address = "Biloxi MS"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Mobile",
+                Address = "Mobile MS"
+            });
+            dataContext.Set<TrainStation>()
+         .Add(new TrainStation
+         {
+             Name = "EnTrack Meridian",
+             Address = "Meridian MS"
+         });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack Yazoo City",
+                     Address = "Yazoo City MS"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Jackson",
+                Address = "Jackson MS"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Hazelhurst",
+                   Address = "Hazelhurst MS"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Brookhaven",
+                Address = "Brookhaven MS"
+            });
+            dataContext.Set<TrainStation>()
+         .Add(new TrainStation
+         {
+             Name = "EnTrack McComb",
+             Address = "McComb MS"
+         });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack Schriever",
+                     Address = "Schriever LA"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack New Iberia",
+                Address = "New Iberia LA"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Lafayette",
+                   Address = "Lafayette LA"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Lake Charles",
+                Address = "Lake Charles LA"
+            });
+
+
+            dataContext.Set<TrainStation>()
+           .Add(new TrainStation
+           {
+               Name = "EnTrack Beaumont",
+               Address = "Beaumont TX "
+           });
+            dataContext.Set<TrainStation>()
+         .Add(new TrainStation
+         {
+             Name = "EnTrack Houston",
+             Address = "Houston TX"
+         });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack Galveston",
+                     Address = "Galveston TX"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Nacogdoches",
+                Address = "Nacogodoches TX"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Prairie View",
+                   Address = "Prairie View TX"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Bryan",
+                Address = "Bryan TX"
+            });
+            dataContext.Set<TrainStation>()
+                       .Add(new TrainStation
+                       {
+                           Name = "EnTrack McGregor",
+                           Address = "McGregor Tx"
+                       });
+            dataContext.Set<TrainStation>()
+         .Add(new TrainStation
+         {
+             Name = "EnTrack Waco",
+             Address = "Waco TX"
+         });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack San Antonio",
+                     Address = "San Antonio TX"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack San Marcos",
+                Address = "San Marcos TX"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Austin ",
+                   Address = "Austin TX"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Temple",
+                Address = "Temple TX"
+            });
+
+
+
+            dataContext.Set<TrainStation>()
+     .Add(new TrainStation
+     {
+         Name = "EnTrack Killeen",
+         Address = "Killen TX"
+     });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack Cleburne",
+                     Address = "Cleburne TX"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Forth Worth",
+                Address = "Forth Worth TX"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Dalals",
+                   Address = "Dallas TX"
+               });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Mesquite",
+                Address = "Mesquite TX"
+            });
+            dataContext.Set<TrainStation>()
+                       .Add(new TrainStation
+                       {
+                           Name = "EnTrack Mineola",
+                           Address = "Mineola Tx"
+                       });
+            dataContext.Set<TrainStation>()
+         .Add(new TrainStation
+         {
+             Name = "EnTrack Longview",
+             Address = "Longview TX"
+         });
+            dataContext.Set<TrainStation>()
+                 .Add(new TrainStation
+                 {
+                     Name = "EnTrack Marshall",
+                     Address = "Marshall TX"
+                 });
+            dataContext.Set<TrainStation>()
+            .Add(new TrainStation
+            {
+                Name = "EnTrack Texarkana",
+                Address = "Texarkana AK"
+            });
+            dataContext.Set<TrainStation>()
+               .Add(new TrainStation
+               {
+                   Name = "EnTrack Shreveport ",
+                   Address = "Shreveport lA"
+               });
+            
+
+
+
         }
 
         await dataContext.SaveChangesAsync();
